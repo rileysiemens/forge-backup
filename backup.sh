a0138c17310323a79cdd95e32bc2ccb21d8a7d0f
Riley Siemens <contact@rileysiemens.com>
2024-10-23 12:13:25 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-serverId= # your server ID goes here
-backupId= # your backup ID goes here
-apiKey="" # your API key goes here
+serverId=123456 # your server ID goes here
+backupId=54321 # your backup ID goes here
+apiKey="your_API_key_goes_here"
 url="https://forge.laravel.com/api/v1/servers/$serverId/backup-configs/$backupId"
 
 timeout=10 # default timout 10 minutes
@@ -14,18 +14,22 @@ echo ". done"
 
 echo -n "Waiting for backup to complete"
 i=0
-while [[ $i -le (6 * $timout) ]]; do
+while (( $i <= 6*$timeout )); do
     sleep 10
     newBackup=$((curl $url -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $apiKey" | jq '.backup.backups | last | .uuid') 2> /dev/null)
     if [[ $oldBackup != $newBackup ]]; then
         echo " done"
-        i=(6 * $timeout)
+        i=$(( 6 * $timeout + 1 )) # End the loop
     else
         echo -n "."
-        ((i+=1))
+        (( i+=1 ))
     fi
 done
 
 if [[ $oldBackup == $newBackup ]]; then
-    echo -e "\nWaited $timeout minutes but the backup didn't finish.\n\nExiting."
+    echo -e "\nWaited $timeout minutes but the backup didn't finish.\n"
+    
+    # uncomment these lines if you want the script to exit on failure
+    # echo -e "\nExiting."
+    # exit 1
 fi
